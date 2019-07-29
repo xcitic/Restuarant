c0e7f429188c3c051289d537ff1c8de6bddd45ce
xcitic <sami@ecompx.com>
2019-07-29 10:36:26 +0200

    Admin user seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(ReservationTableSeeder::class);
         $this->call(MessageTableSeeder::class);
     }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = DB::table('users')->where('type', 'admin')->first();
+
+        if(!$admin) {
+          return DB::table('users')->insert([
+                'name' => 'admin',
+                'email' => 'admin@admin.admin',
+                'type' => 'admin',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+              ]);
+        }
+        return;
+    }
+}
